5edcc83f4254a4d4f0514966147e5f882c06c6af
heliahs <helia.hosseinioun@gmail.com>
2021-03-03 14:58:41 +0000

    before testing

--- app/build/tmp/kapt3/stubs/release/com/hh/coronalastupdate/viewmodels/ListViewModel.java
+++ app/build/tmp/kapt3/stubs/release/com/hh/coronalastupdate/viewmodels/ListViewModel.java
@@ -59,11 +59,12 @@ public final class ListViewModel extends androidx.lifecycle.AndroidViewModel {
         super(null);
     }
     
-    @kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0010\n\u0002\b\u0005\b\u0086\u0001\u0018\u00002\b\u0012\u0004\u0012\u00020\u00000\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002j\u0002\b\u0003j\u0002\b\u0004j\u0002\b\u0005\u00a8\u0006\u0006"}, d2 = {"Lcom/hh/coronalastupdate/viewmodels/ListViewModel$ApiStatus;", "", "(Ljava/lang/String;I)V", "LOADING", "ERROR", "DONE", "app_release"})
+    @kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0010\n\u0002\b\u0006\b\u0086\u0001\u0018\u00002\b\u0012\u0004\u0012\u00020\u00000\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002j\u0002\b\u0003j\u0002\b\u0004j\u0002\b\u0005j\u0002\b\u0006\u00a8\u0006\u0007"}, d2 = {"Lcom/hh/coronalastupdate/viewmodels/ListViewModel$ApiStatus;", "", "(Ljava/lang/String;I)V", "LOADING", "ERROR", "DONE", "TOAST_ERROR", "app_release"})
     public static enum ApiStatus {
         /*public static final*/ LOADING /* = new LOADING() */,
         /*public static final*/ ERROR /* = new ERROR() */,
-        /*public static final*/ DONE /* = new DONE() */;
+        /*public static final*/ DONE /* = new DONE() */,
+        /*public static final*/ TOAST_ERROR /* = new TOAST_ERROR() */;
         
         ApiStatus() {
         }
